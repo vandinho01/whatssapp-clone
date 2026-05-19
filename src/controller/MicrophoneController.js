9dc4a856e93250ea9626e5fd5b207cd6e73ddd8f
Vandinho <evandro.menshaid@gmail.com>
2026-05-19 17:41:09 -0300

    W21 - Ativando o microfone com API MediaDevices.getUserMedia()

---
+++ src/controller/MicrophoneController.js
@@ -0,0 +1,26 @@
+export class MicrophoneController {
+
+    constructor() {
+        navigator.mediaDevices.getUserMedia({
+            audio: true
+        }).then(stream => {
+            this._stream = stream
+
+            let audio = new Audio();
+
+            audio.src = URL.createObjectURL(stream);
+
+            audio.play();
+
+        }).catch(err => {
+            console.error(err);
+        });
+    }
+
+     stop (){
+        this._stream.getTracks().forEach(track=>{
+            track.stop();
+        });
+    }
+
+}
--- src/controller/WhatssappController.js
+++ src/controller/WhatssappController.js
@@ -1,5 +1,6 @@
 import { Format } from './../util/Format';
 import { CameraController } from './CameraController';
+import { MicrophoneController } from './MicrophoneController';
 import { DocumentPreviewController } from './DocumentPreviewController';
 
 export class WhatssappController {
@@ -347,16 +348,20 @@ export class WhatssappController {
             this.el.btnSendMicrophone.hide();
             this.startRecordMicrophoneTime();
 
+            this._microphoneController = new MicrophoneController();
+
         });
         // cancela o audio
         this.el.btnCancelMicrophone.on('click', e => {
 
+            this._microphoneController.stop();
             this.closeRecordMicrophone();
 
         });
         //envia o audio
         this.el.btnFinishMicrophone.on('click', e => {
 
+            this._microphoneController.stop();
             this.closeRecordMicrophone();
 
         });
