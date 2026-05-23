d3e447861b45dacb8e9786592cd39457a4a160f7
Vandinho <evandro.menshaid@gmail.com>
2026-05-23 08:45:54 -0300

    W26 - Firebase Authentication

--- src/controller/WhatssappController.js
+++ src/controller/WhatssappController.js
@@ -8,10 +8,26 @@ export class WhatssappController {
 
     constructor() {
 
+        this._firebase = new Firebase();
+        this.initAuth();  //autenticação
         this.elementsPrototype();
         this.loadElements();
         this.initEvents();
-        this._firebase = new Firebase();
+
+    }
+
+    initAuth(){
+
+        this._firebase.initAuth()
+        .then(response => {
+            this._user = response.user;
+            this.el.appContent.css({
+                display: 'flex'
+            });
+        })
+        .catch(err=>{
+            console.err(err)
+        })
 
     }
 
--- src/util/Firebase.js
+++ src/util/Firebase.js
@@ -1,35 +1,54 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage';
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/storage";
+import "firebase/auth";
 
 export class Firebase {
+  constructor() {
+    this._config = {
+      apiKey: process.env.FIREBASE_API_KEY,
+      authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+      messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+      appId: process.env.FIREBASE_APP_ID,
+      measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+    };
+    this.init();
+  }
 
-    constructor() {
-        this._config = {
-            apiKey: "AIzaSyAbMJAqkj44SIPQHe7Cqk7ouiDsOuuouOU",
-            authDomain: "whatssapp-clone-a31cf.firebaseapp.com",
-            projectId: "whatssapp-clone-a31cf",
-            storageBucket: "whatssapp-clone-a31cf.firebasestorage.app",
-            messagingSenderId: "335772200172",
-            appId: "1:335772200172:web:889f53b8efbe0acbfd09c9",
-            measurementId: "G-S9FC7445BK"
-        };
-        this.init();
+  init() {
+    if (!firebase.apps.length) {
+      Firebase._app = firebase.initializeApp(this._config);
+    } else {
+      Firebase._app = firebase.app();
     }
+  }
 
-    init() {
-        if (!this._initializeApp) {
-            Firebase._app = initializeApp(this._config);
-            this._initializeApp = true;
-        }
-    }
+  static db() {
+    return firebase.firestore();
+  }
 
-    static db() {
-        return getFirestore(Firebase._app);
-    }
+  static hd() {
+    return firebase.storage();
+  }
 
-    static hd() {
-        return getStorage(Firebase._app);
-    }
+  initAuth() {
+    return new Promise((resolve, reject) => {
+      let provider = new firebase.auth.GoogleAuthProvider();
 
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          let token = result.credential.accessToken;
+          let user = result.user;
+
+          resolve({ user, token });
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
+  }
 }
