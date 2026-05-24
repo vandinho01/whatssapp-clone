f5a63d17b42cf3e09ce8ccefff65c13133420f0e
Vandinho <evandro.menshaid@gmail.com>
2026-05-24 13:57:26 -0300

    W28 - Aplicando padrão DAO ao objeto User e criando Evento de dados alterados

--- src/controller/WhatssappController.js
+++ src/controller/WhatssappController.js
@@ -18,31 +18,48 @@ export class WhatssappController {
 
     }
 
-    initAuth(){
+    initAuth() {
 
         this._firebase.initAuth()
-        .then(response => {
-            this._user = new User();
+            .then(response => {
+                this._user = new User(response.user.email);
 
-            let userRef = User.findByEmail(response.user.email);
+                this._user.on('datachange', data => {
 
-            userRef.set({
-                name: response.user.displayName,
-                email: response.user.email,
-                photo: response.user.photoURL
-            }).then(() => {
-                this.el.appContent.css({
-                    display: 'flex'
+                    document.querySelector('title').innerHTML = data.name + ' - WhatsApp Clone'
+
+                    this.el.inputNamePanelEditProfile.innerHTML = data.name
+
+                    if (data.photo) {
+
+                        let photo = this.el.imgPanelEditProfile;
+                        photo.src = data.photo;
+                        photo.show();
+                        this.el.imgDefaultPanelEditProfile.hide();
+
+                        let photo2 = this.el.myPhoto.querySelector('img');
+                        photo2.src = data.photo;
+                        photo2.show();
+
+                    }
+
                 });
-            });
 
-            this.el.appContent.css({
-                display: 'flex'
-            });
-        })
-        .catch(err=>{
-            console.error(err);
-        })
+                this._user.name = response.user.displayName;
+                this._user.email = response.user.email;
+                this._user.photo = response.user.photoURL;
+
+                this._user.save().then(() => {
+
+                    this.el.appContent.css({
+                        display: 'flex'
+                    });
+
+                });
+            })
+            .catch(err => {
+                console.error(err);
+            })
 
     }
 
