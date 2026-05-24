59b7472199bfe9f93b408bcacb80bc14207d8d75
Vandinho <evandro.menshaid@gmail.com>
2026-05-24 14:44:20 -0300

    W31 - Listando os contatos

--- src/model/User.js
+++ src/model/User.js
@@ -47,6 +47,14 @@ export class User extends Model {
 
     }
 
+    static getContactsRef(id){
+
+        return User.getRef()
+            .doc(id)
+            .collection('contacts')
+
+    }
+
     static findByEmail(email){
 
         return User.getRef().doc(email);
@@ -55,11 +63,37 @@ export class User extends Model {
 
     addContact(contact){
 
-        return User.getRef()
-            .doc(this.email)
-            .collection('contacts')
+        return getContactsRef(this.email)
             .doc(btoa(contact.email))
             .set(contact.toJSON());
+
+    }
+
+    getContacts(){
+
+        return new Promise((resolve, reject)=>{
+
+            User.getContactsRef(this.email).onSnapshot(docs => {
+
+                let contacts = [];
+
+                docs.forEach(doc => {
+
+                    let data = doc.data();
+
+                    data.id = doc.id;
+
+                    contacts.push(data);
+
+                });
+
+                this.trigger('contactschange', docs);
+
+                resolve(contacts)
+
+            });
+
+        });
 
     }
 
