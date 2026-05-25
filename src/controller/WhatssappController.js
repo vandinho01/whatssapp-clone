c2c64ea85e5f14e7af0578470b9e3957ca10a86f
Vandinho <evandro.menshaid@gmail.com>
2026-05-24 21:40:27 -0300

    W39 - Atualizando status de mensagem (wait, sent e read)

--- src/controller/WhatssappController.js
+++ src/controller/WhatssappController.js
@@ -193,17 +193,29 @@ export class WhatssappController {
                 let data = doc.data();
                 data.id = doc.id;
 
+                let message = new Message();
+                message.fromJSON(data);
+
+                let me = (data.from === this._user.email);
+
                 if(!this.el.panelMessagesContainer.querySelector('#_' + data.id)){
-                    let message = new Message();
 
-                    message.fromJSON(data);
-
-                    let me = (data.from === this._user.email);
+                    if(!me){
+                        doc.ref.set({
+                            status: 'read'
+                        }, {
+                            merge: true
+                        });
+                    }
 
                     let view = message.getViewElement(me);
 
                     this.el.panelMessagesContainer.appendChild(view);
 
+                } else if (me){
+                    let msgEl =  this.el.panelMessagesContainer.querySelector('#_' + data.id)
+
+                    msgEl.querySelector('.message-status').innerHTML = message.getStatusViewElement().outerHTML;
                 }
 
             });
